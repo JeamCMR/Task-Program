06d4ac2a846cdbdf261a54eedaf421abb2568b5f
COPSTA <tutomx16@gmail.com>
2024-04-03 17:13:52 -0500

    Se hacen refacoriza funciones y eventos

--- js/app.js
+++ js/app.js
@@ -23,38 +23,14 @@ const descriptionInput = document.getElementById("description-input"); //Text Ar
 const taskData = [];
 let currentTask = {};
 
- //Eventos
-//Mostrar el formulario al darle clic en el boton agregar tarea
-openTaskFormBtn.addEventListener("click", () => {
-    taskForm.classList.toggle("hidden"); //El metodo classList.toggle (alternancia) agrega una clase
-    //cuando esta no esta presente en el elemnto y elimina una clase cuando esta esta en el elemento
-  });
-  
-  //Mostrar modal (dialog) al dar click en el boton x (salir)
-  closeTaskFormBtn.addEventListener("click", () => {
-    confirmCloseDialog.showModal();
-  });
-  
-  //Cerrar cuadro de dialogo al dar click en el boton Cancelar
-  cancelBtn.addEventListener("click", () => {
-    confirmCloseDialog.close();
-  });
-  
-  //Cerrar cuadro de dialogo y esconder el formulario al dar click en el boton descartar
-  discardBtn.addEventListener("click", () => {
-    confirmCloseDialog.close();
-    taskForm.classList.toggle("hidden");
-  });
-  
-  //Guardar variables en el array taskData y mostrarlas en el html
-  taskForm.addEventListener("submit", (e) => {
-    e.preventDefault(); // no actualizar la pagina cuando ese envie la informacion del formulario
-  
-    //El metodo.findeIndex recorre un array y return el indice del primer elemento que cumpla con
+ /*Eventos y funciones*/
+
+ //Guardar variables en el array taskData  y valida si el el id existe
+const addOrUpdateTask  = () =>{
+      //El metodo.findeIndex recorre un array y return el indice del primer elemento que cumpla con
     //los creterios especificados en la funcion callback, si no encuentran un elemento return -1
     const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-    console.log("INDICE: " + dataArrIndex);
-  
+    // console.log("INDICE: " + dataArrIndex);
     const taskObj = {
       //se crea un objeto con los valores de los input.
       //el id es el valor del titleinput en minusculas separado por un "-" y le agregra el valor en milisegundos al final para que sea unico
@@ -68,20 +44,75 @@ openTaskFormBtn.addEventListener("click", () => {
       //si no  agrega el objeto al comienzo del array
       taskData.unshift(taskObj); //unshift() es un método de matriz que se utiliza para agregar uno o más elementos al comienzo de una matriz.
     }
-  
+
+    updateTaskContainer();
+    reset();
+}
+
+
+
+const updateTaskContainer = () =>{
+    tasksContainer.innerHTML="";  //Limpia el html antes de recorer el array
     taskData.forEach(({ id, title, date, description }) => {
-      tasksContainer.innerHTML += `
-          <div class="task" id="${id}">
-              <p><strong>Title:</strong>${title}</p>
-              <p><strong>Date:</strong>${date}</p>
-              <p><strong>Description:</strong>${description}</p>
-              <button type="button" class="btn">Edit</button>
-              <button type="button" class="btn">Delete</button>
-          </div>
-          `;
-    });
+        tasksContainer.innerHTML += `
+            <div class="task" id="${id}">
+                <p><strong>Title:</strong>${title}</p>
+                <p><strong>Date:</strong>${date}</p>
+                <p><strong>Description:</strong>${description}</p>
+                <button type="button" class="btn">Edit</button>
+                <button type="button" class="btn">Delete</button>
+            </div>
+            `;
+      });
+}
+
+
+
+ //Limpiar campos de input
+const reset = () => {
+    titleInput.value="";
+    dateInput.value="";
+    descriptionInput.value="";
+
     taskForm.classList.toggle("hidden");
+    currentTask = {};  
+}
+
+
+//Mostrar el formulario al darle clic en el boton agregar tarea
+openTaskFormBtn.addEventListener("click", () => {
+    taskForm.classList.toggle("hidden"); //El metodo classList.toggle (alternancia) agrega una clase
+    //cuando esta no esta presente en el elemnto y elimina una clase cuando esta esta en el elemento
+  });
+  
+  //Mostrar modal (dialog) al dar click en el boton x (salir)
+  closeTaskFormBtn.addEventListener("click", () => {
+     //validar si en los input hay algun texto
+     const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value ;
+     if (formInputsContainValues) {
+        confirmCloseDialog.showModal(); 
+     }else{
+        reset();
+     }
+      
+  });
+  
+  //Cerrar cuadro de dialogo al dar click en el boton Cancelar
+  cancelBtn.addEventListener("click", () => {
+    confirmCloseDialog.close();
+  });
+  
+  //Cerrar cuadro de dialogo y esconder el formulario al dar click en el boton descartar
+  discardBtn.addEventListener("click", () => {
+    confirmCloseDialog.close();
+    reset();
+  });
+  
+  // Mostrar las tareas en el html
+  taskForm.addEventListener("submit", (e) => {
+    e.preventDefault(); // no actualizar la pagina cuando ese envie la informacion del formulario
+    addOrUpdateTask();
     //  console.log(taskObj.id);
-    // console.log(taskData);
+    console.log(taskData);
   });
   
